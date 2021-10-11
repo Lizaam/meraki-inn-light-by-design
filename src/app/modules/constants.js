f4687f20dd8042b1db560e9d5c3cc90816115b04
Lizaam <lizaame@bumblebee.co.za>
2021-10-11 08:42:54 +0200

    continued contact form development

--- src/app/modules/constants.js
+++ src/app/modules/constants.js
@@ -37,4 +37,8 @@ export default class CONSTANTS {
     moreThan8x: 'more than 8x',
     otherFittings: 'other fittings'
   }
+
+  static googleSrc = {
+    src: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3309.7948409665373!2d18.551015214781184!3d-33.94640443052128!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1dcc5b3bcce3cfa5%3A0x6c7dae69f66ecf7!2s42%20Kameeldoring%20Street%2C%20Bonteheuwel%2C%20Cape%20Town%2C%207764!5e0!3m2!1sen!2sza!4v1591617549743!5m2!1sen!2sza'
+  }
 }
--- src/app/pages/contact/contact.component.js
+++ src/app/pages/contact/contact.component.js
@@ -3,6 +3,7 @@ import { Container, Row, Col, Form, Button, ListGroup } from 'react-bootstrap';
 import './contact.component.scss';
 import PageIndication from '../../components/page-indication/page-indication.component';
 import { SendContactForm } from '../../providers/emails.provider';
+import CONSTANTS from '../../modules/constants';
 
 const ContactComponent = () => {
   const [name, setName] = useState('');
@@ -35,7 +36,7 @@ const ContactComponent = () => {
           <Col>
             <div>
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3309.7948409665373!2d18.551015214781184!3d-33.94640443052128!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1dcc5b3bcce3cfa5%3A0x6c7dae69f66ecf7!2s42%20Kameeldoring%20Street%2C%20Bonteheuwel%2C%20Cape%20Town%2C%207764!5e0!3m2!1sen!2sza!4v1591617549743!5m2!1sen!2sza"
+                src={CONSTANTS.googleSrc.src}
                 width="100%"
                 height="300px"
                 frameBorder="none"
